26339b0f39d855ba72fd00b447c98e441e04f716
Artisanter <chyzhyk99@gmail.com>
2019-12-11 07:22:44 +0300

    SignalR

---
+++ StudentNetwork/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using StudentNetwork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentNetwork
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(Message message)
+        {
+            await Clients.All.SendAsync("Send", message).ConfigureAwait(true);
+        }
+    }
+}
