115d4c3e49ec0d3fdb6c6b145b50beb9ad757a83
Artisanter <chyzhyk99@gmail.com>
2019-12-11 07:20:58 +0300

    TagHelpers

--- StudentNetwork/TagHelpers/PictureTagHelper.cs
+++ StudentNetwork/TagHelpers/PictureTagHelper.cs
@@ -7,14 +7,20 @@ using StudentNetwork.Models;
 
 namespace StudentNetwork.TagHelpers
 {
-    public class ImageTagHelper : TagHelper
+    public class PictureTagHelper : TagHelper
     {
         public Image Image{get;set;}
-
+        public bool Round { get; set; }
         public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            if(Image != null && Image.Bytes != null)
+            if (Image != null && Image.Bytes != null && output != null)
+            {
+                output.TagName = "img";
+                output.TagMode = TagMode.SelfClosing;
                 output.Attributes.SetAttribute("src", $"data:image/jpeg;base64,{Convert.ToBase64String(Image.Bytes)}");
+                if (Round)
+                    output.Attributes.SetAttribute("style", "width: 100px;height: 100px;border-radius: 50%;");
+            }
             return base.ProcessAsync(context, output);
         }
     }
